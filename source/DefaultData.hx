26409e3329d6ee99e285bb5cfc2229d867c368a6
BTF2021 <101050933+BTF2021@users.noreply.github.com>
2022-04-15 21:26:37 +0300

    0.1.1

--- source/DefaultData.hx
+++ source/DefaultData.hx
@@ -15,5 +15,29 @@ class DefaultData
 	    
 		if (FlxG.save.data.sistemInvert == null)
             FlxG.save.data.sistemInvert = false;
+		if (FlxG.save.data.sistemOption == null)
+			FlxG.save.data.sistemOption = 1;
+
+		if (FlxG.save.data.optionsZecimal == null)
+			FlxG.save.data.optionsZecimal = false;
+	    if (FlxG.save.data.optionsModifier == null)
+			FlxG.save.data.optionsModifier = true;
+		if (FlxG.save.data.optionsGreg == null)
+			FlxG.save.data.optionsGreg = false;
+		if (FlxG.save.data.optionsAnimation == null)
+			FlxG.save.data.optionsAnimation = true;
+		if (FlxG.save.data.optionsSkipTitle == null)
+			FlxG.save.data.optionsSkipTitle = false;
+		if (FlxG.save.data.optionsInfo == null)
+			FlxG.save.data.optionsInfo = true;
+		if (FlxG.save.data.optionsEaster == null)
+			FlxG.save.data.optionsEaster == false;
+	    if (FlxG.save.data.optionsUpdate == null)
+			FlxG.save.data.optionsUpdate = true;
+		if (FlxG.save.data.optionsShutUp == null)
+		{
+			if (FlxG.random.bool(0.1)) FlxG.save.data.optionsShutUp = true;
+			else FlxG.save.data.optionsShutUp = false;
+		}
 	}
 }
